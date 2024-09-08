a729cb4e25b455d14f80f307d6e90ef2a6d6e336
vsskx <76752087+vsskx@users.noreply.github.com>
2024-09-08 18:06:43 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,20 @@ with open("README.md", "r") as file:
     readme = file.read()
 
 setup(
-    name="marzpy",
-    version="0.0.5",
+    name="marzpy-fork", 
+    version="0.0.6",
     author="Mewhrzad",
-    description="a simple application with python to manage Marzban panel",
-    long_description="text/markdown",
-    url="https://github.com/Mewhrzad/marzpy",
+    description="A fork of the original Marzpy library to manage Marzban panel",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mewhrzad/marzpy-fork",  # URL на твой форк
     keywords=["marzpy", "Marzban", "Gozargah", "Marzban python", "Marzban API"],
     packages=find_packages(),
     install_requires=["aiohttp"],
-    classifiers=["Programming Language :: Python :: 3"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
